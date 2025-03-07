1c5960dc39196f740595a034189374ad7d600f72
pankajmutha14 <50906692+pankajmutha14@users.noreply.github.com>
2025-03-07 19:16:45 +0530

    Update PsaGitExamplesApplication.java

--- src/main/java/com/psa/PsaGitExamplesApplication.java
+++ src/main/java/com/psa/PsaGitExamplesApplication.java
@@ -6,7 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PsaGitExamplesApplication {
 
+	
+
 	public static void main(String[] args) {
+		int x = 100;
 		SpringApplication.run(PsaGitExamplesApplication.class, args);
 	}
 
